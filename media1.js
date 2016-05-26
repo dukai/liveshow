1cd3885c034f97945cc49f30002fadf8fb85916d
Kyle <dukai86@gmail.com>
2016-05-27 00:14:15 +0800

    update buffer

--- media1.js
+++ media1.js
@@ -9,21 +9,46 @@ var videoElem = document.getElementById("video");
 
 var mediaSource = new MediaSource();
 var sourceBuffer;
+var buffers = [];
+var updating = false;
+var bufferCount = 0;
+var startedPlay = false;
 
 videoElem.src = URL.createObjectURL(mediaSource);
 //var mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
 var mimeCodec = 'video/webm; codecs="vp8,vorbis"';
 
+
+var notify = function(){
+  if(updating || buffers.length == 0){
+    return false;
+  }
+  updating = true;
+  var buffer = buffers.shift();
+  sourceBuffer.appendBuffer(buffer);
+}
+
 mediaSource.addEventListener('sourceopen', function(event){
   sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
   sourceBuffer.addEventListener('updateend', function(){
-    videoElem.play();
+    updating = false;
+    if(!startedPlay){
+      bufferCount += 1;
+      if(bufferCount > 2){
+        startedPlay = true;
+        videoElem.play();
+      }
+    }
+    notify();
   });
 });
 
 var reader = new FileReader();
 reader.addEventListener('load', function(e){
-  sourceBuffer.appendBuffer(new Uint8Array(e.target.result));
+  console.log(new Uint8Array(e.target.result));
+  buffers.push(new Uint8Array(e.target.result));
+  //sourceBuffer.appendBuffer(new Uint8Array(e.target.result));
+  notify();
 });
 
 navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
@@ -36,25 +61,10 @@ navigator.getUserMedia({video: true},function(stream){
     reader.readAsArrayBuffer(event.data);
   });
 
-  mediaRecorder.start(200);
+  mediaRecorder.start(5000);
 	//videoElem.src = URL.createObjectURL(stream);
 	//videoElem.play();
 }, function(error){
 	console.log(error);
 });
 
-//navigator.mediaDevices.getUserMedia(
-//  { video: true }
-//).then(
-//  function(stream) {
-//    //videoElem.play();
-//
-//    var mediaRecorder = new MediaRecorder(stream);
-//    mediaRecorder.addEventListener('dataavailable', function(event){
-//      console.log(event);
-//      reader.readAsArrayBuffer(event.data);
-//    });
-//
-//    mediaRecorder.start(2000);
-//  }
-//);
